0ecf100f537b7eb10212429a8fc8031cd8a05ccd
David Wrighton <davidwr@microsoft.com>
2019-09-25 18:04:56 -0700

    Crossgen/tibcmgr placed into correct location, and use --resilient switch

--- build-coreclr.sh
+++ build-coreclr.sh
@@ -11,6 +11,7 @@ coreclrbinariesdir="$scriptroot/webapi/coreclrbin"
 # apt install cmake clang llvm libicu-dev liblttng-ust-dev libkrb5-dev
 
 # place binaries into a separate directory for submission to perflab
+rm -r -f webapi/coreclrbin
 mkdir -p webapi/coreclrbin
 cp "$coreclrbuilddir/bundle.dll" "$coreclrbinariesdir"
 cp "$coreclrbuilddir/bundle.runtimeconfig.json" "$coreclrbinariesdir"
@@ -19,7 +20,7 @@ cp "$coreclrbuilddir/corebundle" "$coreclrbinariesdir"
 cp "$coreclrbuilddir/System.Private.CoreLib.dll" "$coreclrbinariesdir"
 rm -r -f webapi/coreclrbin/crossgen2
 mkdir -p webapi/coreclrbin/crossgen2
-cp $coreclrbuilddir_rel/crossgen2published/* "$coreclrbinariesdir/crossgen2"
+cp $coreclrbuilddir_rel/crossgen2/* "$coreclrbinariesdir/crossgen2"
 rm -r -f webapi/coreclrbin/tibcmgr
 mkdir -p webapi/coreclrbin/tibcmgr
-cp $coreclrbuilddir_rel/tibcmgrpublished/* "$coreclrbinariesdir/tibcmgr"
+cp $coreclrbuilddir_rel/tibcmgr/* "$coreclrbinariesdir/tibcmgr"
